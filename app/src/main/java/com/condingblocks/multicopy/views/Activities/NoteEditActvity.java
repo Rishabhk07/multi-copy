16aee8fe08e9d9e3dc89b16d516a37fed7b743a8
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-05-23 20:19:26 +0530

    home up button color changed to white
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/condingblocks/multicopy/views/Activities/NoteEditActvity.java
+++ app/src/main/java/com/condingblocks/multicopy/views/Activities/NoteEditActvity.java
@@ -3,6 +3,7 @@ package com.condingblocks.multicopy.views.Activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
